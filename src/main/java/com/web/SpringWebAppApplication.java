eca4e707d956ba413092463fed8882c2406646fa
cvenkatesulu <cvenkatesulu9@gemail.com>
2021-07-25 21:05:22 +0530

    added logging mechanism

--- src/main/java/com/web/SpringWebAppApplication.java
+++ src/main/java/com/web/SpringWebAppApplication.java
@@ -1,11 +1,14 @@
 package com.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplicat
 
+        @Slf4j
 @SpringBootApplication
 public class SpringWebAppApplication {
 
+    log.info("Added logging mechanism")
     public static void main(String[] args) {
         SpringApplication.run(SpringWebAppApplication.class, args);
     }
