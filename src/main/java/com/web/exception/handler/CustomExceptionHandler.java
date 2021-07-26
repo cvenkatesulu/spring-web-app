c39c64019b10dc22a0f341e4331284a5ef41e52e
cvenkatesulu <cvenkatesulu9@gemail.com>
2021-07-26 21:45:36 +0530

    add exception

---
+++ src/main/java/com/web/exception/handler/CustomExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.web.exception.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> userNotFoundHandler(){
+
+    }
+
+}
